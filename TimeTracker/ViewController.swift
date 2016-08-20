df87c7d784cbffb66ce54e6e29cbb45d97223c48
Kyle Couch <Kyle@kyles-macbook-pro.local>
2016-08-20 13:39:25 -0400

    Initial Commit

---
+++ TimeTracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TimeTracker
+//
+//  Created by Kyle Couch on 8/20/16.
+//  Copyright © 2016 Kyle Couch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
